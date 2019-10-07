689ca015deb27d87e7296da4e4d3fb6da287cd2a
Francisco Rafart <francisco.rafart@gmail.com>
2019-10-07 16:11:34 -0400

    Rename env variables

--- nextjs/client.js
+++ nextjs/client.js
@@ -2,7 +2,7 @@ import sanityClient from '@sanity/client'
 
 // TODO: Set them as env variables, hide them and deploy
 export default sanityClient({
-    projectId: process.env.projectId,
-    dataset: process.env.dataset,
-    useCdn: process.env.useCdn
+    projectId: process.env.PROJECTID,
+    dataset: process.env.DATASET,
+    useCdn: process.env.USECDN
 })
