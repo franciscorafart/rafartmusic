fa98329bca948871357823527bd269cc28daba66
Francisco Rafart <francisco.rafart@gmail.com>
2019-10-11 10:27:14 -0400

    Add google analytics script

--- nextjs/src/IndexPage.js
+++ nextjs/src/IndexPage.js
@@ -13,6 +13,16 @@ class IndexPage extends Component {
                     <meta name="description" content="Rafart Champan stick music"/>
                     <meta name="keywords" content="Rafart,Chapman Stick,Electronic Rock,Progressive rock"/>
                     <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-79781872-1"></script>
+
+                    <script>{`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'UA-79781872-1');
+                    `}
+                    </script>
+
                     <link href="https://fonts.googleapis.com/css?family=Encode+Sans+Expanded&display=swap" rel="stylesheet"/>
                     <link rel="shortcut icon" type="image/png" href={this.props.urlFor(this.props.favicon.mainImage.asset._ref)}/>
                 </Head>
