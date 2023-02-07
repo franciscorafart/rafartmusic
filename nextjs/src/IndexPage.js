31a9db7cba6be11091ec54c98b2b1966e61b1a88
franciscorafart <francisco.rafart@gmail.com>
2023-02-06 21:41:27 -0500

    Migrate google analytics

--- nextjs/src/IndexPage.js
+++ nextjs/src/IndexPage.js
@@ -9,16 +9,16 @@ const IndexPage = props =>  {
                 <meta name="description" content="Rafart Champan stick music"/>
                 <meta name="keywords" content="Rafart,Chapman Stick,Electronic Rock,Progressive rock"/>
                 <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-                <script async src="https://www.googletagmanager.com/gtag/js?id=UA-79781872-1"></script>
                 <script src="https://unpkg.com/embeddable-nfts/dist/nft-card.min.js"></script>
 
+                <script async src="https://www.googletagmanager.com/gtag/js?id=G-3F3QGZG209"></script>
                 <script>{`
                     window.dataLayer = window.dataLayer || [];
                     function gtag(){dataLayer.push(arguments);}
                     gtag('js', new Date());
-                    gtag('config', 'UA-79781872-1');
+                    gtag('config', 'G-3F3QGZG209');
                 `}
-                </script>
+</script>
 
                 <link href={`https://fonts.googleapis.com/css?family=Encode+Sans+Expanded&display=swap`} rel="stylesheet"/>
                 {props.urlFor && props.favicon && <link rel="shortcut icon" type="image/png" href={`${props.urlFor(props.favicon.mainImage.asset._ref).url()}`}/>}
