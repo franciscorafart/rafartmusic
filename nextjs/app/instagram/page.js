45143b142568c31baad0c5a4cda458a5e7a5235e
franciscorafart <francisco.rafart@gmail.com>
2025-01-14 18:36:30 -0500

    Update instagram landing

--- nextjs/app/instagram/page.js
+++ nextjs/app/instagram/page.js
@@ -2,14 +2,10 @@ import './style.css';
 
 const Links = () => {
         const links = [
-            {url: 'https://open.spotify.com/track/7dNB3n5q67Yr4bkt6HZPXv?si=1998f79ec05c44f9', label: 'Thanatos (Intrumental)'},
-            {url: 'https://open.spotify.com/track/1GDq44rl1KaqFHTP7lAMaF?si=11a8e7261a114b8d', label: 'Synapsis III (Franciscart)'},
-            {url: 'https://open.spotify.com/album/1fL2QgDiPrtoGAtJm8t7EQ?si=jAZA7wXCRu6Vz583GAkirg', label: 'Zappateada II (Instrumental)'},
-            {url: 'https://app.rafartmusic.com/alienation-dance', label: 'Alienation Dance Web Song'},
-            {url: '/catalogue', label: 'Catalogue'},
+            {url: 'https://rafartmusic.com/album', label: 'Albums and Releases'},
             {url: '/', label: 'Rafart Website'},
             {url: 'https://www.youtube.com/franciscorafart', label: 'Youtube'},
-            {url: 'https://open.spotify.com/artist/3EIxblOsWLuH54ZRWDuw1m?si=bg0n-AiKSFOhQ_cJmMszUw&nd=1', label: 'Spotify'},
+            {url: 'https://app.rafartmusic.com/alienation-dance', label: 'Alienation Dance Web Song'},
         ];
 
         return (
