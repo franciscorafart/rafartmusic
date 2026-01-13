355ae2bc38473950f48b397d6e31982da1217dfb
franciscorafart <francisco.rafart@gmail.com>
2026-01-13 14:54:55 -0500

    Redirect albums path

--- nextjs/next.config.js
+++ nextjs/next.config.js
@@ -5,5 +5,14 @@ module.exports = {
     dataset: process.env.dataset,
     useCdn: process.env.useCdn,
     apiVersion: process.env.apiVersion,
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/albums',
+        destination: '/music',
+        permanent: true,
+      },
+    ];
+  },
 };
